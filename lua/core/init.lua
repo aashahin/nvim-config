7138c8b4a3d2a69489d366d62e2b59dc544d39ee
Abdelrahman Shaheen <almhtrfahmed123@gmail.com>
2023-02-28 19:33:13 +0200

    init

---
+++ lua/core/init.lua
@@ -0,0 +1,6 @@
+-- options & settings for neovim
+require('core.options')
+-- Keybindings
+require('core.mappings')
+-- Neovide users 
+require('core.neovide')
---
+++ lua/plugins/init.lua
@@ -0,0 +1,176 @@
+local ok, lazy = pcall(require, "lazy")
+if not ok then
+	return
+end
+
+lazy.setup({
+	install = { colorscheme = { "catppuccin", "habamax" } },
+	root = vim.fn.stdpath("data") .. "/lazy",
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- LSP Stuff
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"jose-elias-alvarez/null-ls.nvim",
+			"glepnir/lspsaga.nvim",
+			"RRethy/vim-illuminate",
+
+			-- Completions
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lua",
+			"lukas-reineke/cmp-under-comparator",
+			"L3MON4D3/LuaSnip",
+			"hrsh7th/cmp-vsnip",
+			"rafamadriz/friendly-snippets",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+		},
+		config = function()
+			-- Lsp Configs
+			require("plugins.configs.lsp")
+			-- Completions
+			require("plugins.configs.cmp")
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("plugins.configs.colorschemes")
+		end,
+	},
+	{ "catppuccin/nvim", name = "catppuccin" },
+	{
+		"akinsho/bufferline.nvim",
+		dependencies = {
+			"kyazdani42/nvim-web-devicons",
+		},
+		config = function()
+			require("plugins.configs.ui.bufferline")
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"kyazdani42/nvim-web-devicons",
+		},
+		config = function()
+			require("plugins.configs.ui.lualine")
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			"ravenxrz/DAPInstall.nvim",
+		},
+		config = function()
+			require("plugins.configs.ui.dap")
+		end,
+	},
+	{
+		"utilyre/barbecue.nvim",
+		name = "barbecue",
+		version = "*",
+		dependencies = {
+			"SmiteshP/nvim-navic",
+		},
+		opts = require("plugins.configs.lsp.bbq"),
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+		},
+		keys = {
+			{ "<leader>e", "<cmd>NeoTreeFocusToggle<cr>", desc = "NeoTree" },
+		},
+		config = function()
+			require("plugins.configs.ui.neotree")
+		end,
+	},
+
+	{ "lukas-reineke/indent-blankline.nvim", event = "BufEnter" },
+	{
+		"alvan/vim-closetag",
+		config = function()
+			vim.g.closetag_filenames = "*.html,*.xhtml,*.phtml"
+			vim.g.closetag_xhtml_filenames = "*.xhtml,*.jsx"
+		end,
+	},
+	{ "windwp/nvim-autopairs", config = true, event = "InsertEnter" },
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+		},
+		config = function()
+			require("plugins.configs.telescope")
+		end,
+	},
+
+	{ "numToStr/Comment.nvim", config = true, event = "BufEnter" },
+	{ "lewis6991/gitsigns.nvim", config = true, event = "BufEnter" },
+	{
+		"kdheepak/lazygit.nvim",
+		keys = {
+			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "NeoTree" },
+		},
+	},
+
+	{
+		"karb94/neoscroll.nvim",
+		event = "BufEnter",
+		config = function()
+			require("neoscroll").setup({})
+		end,
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		config = function()
+			require("plugins.configs.ui.toggleterm")
+		end,
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
+	{
+		"petertriho/nvim-scrollbar",
+		config = function()
+			require("plugins.configs.ui.scrollbar")
+		end,
+	},
+	{
+		"goolord/alpha-nvim",
+		config = function()
+			require("plugins.configs.ui.alpha")
+		end,
+	},
+	{
+		"roobert/search-replace.nvim",
+		config = function()
+			require("plugins.configs.search")
+		end,
+	},
+	{
+		"lewis6991/impatient.nvim",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		configs = function()
+			vim.cmd([[TSInstall all]])
+		end,
+	},
+	{ "windwp/nvim-ts-autotag" },
+})
